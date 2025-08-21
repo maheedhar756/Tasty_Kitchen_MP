9ccda725e017733ea1719b826067466824bba79a
Maheedhar <71335660+maheedhar756@users.noreply.github.com>
2025-08-21 12:18:15 +0530

    Fix Cart Context and Import Issues

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { Routes, Route } from 'react-router-dom'
 import ProtectedRoute from './components/ProtectedRoute'
 import Home from './pages/Home'
 import Login from './pages/Login'
-import CartProvider from './pages/CartContext'
+import Cart from './pages/Cart'
 import RestaurantDetails from './pages/RestaurantDetails'
 import NotFound from './pages/NotFound'
+import { CartProvider } from './context/CartContext'
 
 import './App.css'
 
