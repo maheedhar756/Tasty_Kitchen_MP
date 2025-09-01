fc9ffa372070fb7e8890d2f70edbc4fa90a069d2
Maheedhar <maheedharambadipudi756@gmail.com>
2025-09-01 15:13:56 +0530

    25th commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,22 @@ import RestaurantDetails from './pages/RestaurantDetails'
 import NotFound from './pages/NotFound'
 import { CartProvider } from '../src/context/CartContext'
 import OrderPlaced from './pages/OrderPlaced'
-import { Analytics } from "@vercel/analytics/next"
 
 import './App.css'
 
 function App() {
 
   return (
-      <>
-        <CartProvider>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-            <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
-            <Route path="/restaurant/:restrauntId" element={<ProtectedRoute><RestaurantDetails /></ProtectedRoute>} />
-            <Route path="/order-placed" element={<ProtectedRoute><OrderPlaced /></ProtectedRoute>} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </CartProvider>
-        <Analytics />
-      </>
+      <CartProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
+          <Route path="/restaurant/:restrauntId" element={<ProtectedRoute><RestaurantDetails /></ProtectedRoute>} />
+          <Route path="/order-placed" element={<ProtectedRoute><OrderPlaced /></ProtectedRoute>} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </CartProvider>
   )
 }
 
