ea4e6e629aef8b979f086f8b564a98c5e18f28b4
Maheedhar <maheedharambadipudi756@gmail.com>
2025-10-07 10:22:42 +0530

    26th

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import Cookies from 'js-cookie'
 import Navbar from '../components/Navbar'
-import OffersCarousel from '../components/OffersCarousel'
+import OffersCarousel from "../components/OffersCarousel"
 import RestaurantCards from '../components/RestaurantCards'
 import Footer from '../components/Footer'
 import Loader from '../components/Loaders'
